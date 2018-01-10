75f4949a5266ca5b22245b61d4532c58afb793f8
Joshua <joshuamoore555@gmail.com>
2018-01-10 01:23:33 +0000

    voices changes, adding more icons, icons layout changes, custom images

--- EyeTalk/MainWindow.xaml.cs
+++ EyeTalk/MainWindow.xaml.cs
@@ -23,12 +23,17 @@ namespace EyeTalk
         public static int pageNumber = 0;
         public static int amountSelected = 0;
         public static int sentencePicked = 0;
+        
+        List<Picture> categoryData;
+        List<string> savedSentences;
+        SortedList<String, List<Picture>> categories;
+        List<Picture> customPictures;
+
+        List<Image> images;
+        List<Button> buttons;
+        List<TextBlock> textBlocks;
 
         SaveFileSerialiser initialiser = new SaveFileSerialiser();
-        List<Picture> categoryData;
-        SortedList<String, List<Picture>> categories;
-        List<string> savedSentences;
-
         SpeechSynthesizer synth = new SpeechSynthesizer();
         OrderedDictionary sentence = new OrderedDictionary();
         
@@ -36,7 +41,10 @@ namespace EyeTalk
         {
             InitializeComponent();
             synth.Volume = 100;
-            synth.Rate = -2;
+            synth.Rate = 0;
+            categories = initialiser.LoadCategories();
+            savedSentences = initialiser.LoadSentences();
+            customPictures = initialiser.LoadCustomPictures();
         }
 
         /*
@@ -47,7 +55,16 @@ namespace EyeTalk
         {
             categories = initialiser.LoadCategories();
             savedSentences = initialiser.LoadSentences();
+            customPictures = initialiser.LoadCustomPictures();
 
+            categories.Remove("Custom");
+            categories.Add("Custom", customPictures);
+            
+
+            images = new List<Image> {Image1, Image2, Image3, Image4 , Image5, Image6, Image7, Image8, Image9, Image10, Image11, Image12, Image13, Image14, Image15 };
+            buttons = new List<Button> {Image1_Button, Image2_Button, Image3_Button, Image4_Button ,Image5_Button , Image6_Button , Image7_Button , Image8_Button,Image9_Button , Image10_Button,Image11_Button,Image12_Button,Image13_Button,Image14_Button,Image15_Button };
+            textBlocks = new List<TextBlock> {Text1, Text2, Text3, Text4, Text5, Text6, Text7, Text8, Text9, Text10, Text11, Text12, Text13, Text14, Text15};
+       
             var category = categories.ElementAt(pageNumber);
 
             pageNumber = 0;
@@ -64,7 +81,6 @@ namespace EyeTalk
         private void Options_Click(object sender, RoutedEventArgs e)
         {
             myTabControl.SelectedIndex = 2;
-
         }
 
         private void Add_PictureCategory_Click(object sender, RoutedEventArgs e)
@@ -125,113 +141,10 @@ namespace EyeTalk
             }
         }
 
-        private void CreateCategory(KeyValuePair<String, List<Picture>> category)
+        private void Save_Button_Click(object sender, RoutedEventArgs e)
         {
-            CategoryTitle.Text = category.Key;
-            categoryData = category.Value;
-
-            var filepath1 = categoryData.ElementAt(0).FilePath;
-            var filepath2 = categoryData.ElementAt(1).FilePath;
-            var filepath3 = categoryData.ElementAt(2).FilePath;
-
-            var pictureName1 = categoryData.ElementAt(0).Name;
-            var pictureName2 = categoryData.ElementAt(1).Name;
-            var pictureName3 = categoryData.ElementAt(2).Name;
-
-            Image1.Source = new BitmapImage(new Uri(filepath1));
-            Image2.Source = new BitmapImage(new Uri(filepath2));
-            Image3.Source = new BitmapImage(new Uri(filepath3));
-
-            Text1.Text = pictureName1;
-            Text2.Text = pictureName2;
-            Text3.Text = pictureName3;
-
-            if (categoryData.ElementAt(0).Selected == false)
-            {
-                Unhighlight(ButtonImage1);
-            }
-            else
-            {
-                Highlight(ButtonImage1);
-            }
-
-            if (categoryData.ElementAt(1).Selected == false)
-            {
-                Unhighlight(ButtonImage2);
-
-            }
-            else
-            {
-                Highlight(ButtonImage2);
-            }
-
-            if (categoryData.ElementAt(2).Selected == false)
-            {
-                Unhighlight(ButtonImage3);
-            
-            }
-            else
-            {
-                Highlight(ButtonImage3);
-            }
-
-
-        }
-
-        private void ButtonImage1_Click(object sender, RoutedEventArgs e)
-        {
-            var word = Text1.Text;
-            int index = 0;
-            var selected = categoryData.ElementAt(index).Selected;
-           
-            if (amountSelected < 3 && selected == false)
-            {
-                AddWordToSentence(word, index);
-                Highlight(ButtonImage1);
-            }
-            else if(selected == true)
-            {
-                RemoveWordFromSentence(word, index);
-                Unhighlight(ButtonImage1);
-            }
-
-        }
-
-        private void ButtonImage2_Click(object sender, RoutedEventArgs e)
-        {
-            var word = Text2.Text;
-            int index = 1;
-            var selected = categoryData.ElementAt(index).Selected;
-
-            if (amountSelected < 3 && selected == false)
-            {
-                AddWordToSentence(word, index);
-                Highlight(ButtonImage2);
-            }
-            else if (selected == true)
-            {
-                RemoveWordFromSentence(word, index);
-                Unhighlight(ButtonImage2);
-            }
-        }
-
-        private void ButtonImage3_Click(object sender, RoutedEventArgs e)
-        {
-            var word = Text3.Text;
-            int index = 2;
-            var selected = categoryData.ElementAt(index).Selected;
-          
-            if (amountSelected < 3 && selected == false)
-            {
-                AddWordToSentence(word, index);
-                Highlight(ButtonImage3);
-            }
-            else if (selected == true)
-            {
-                RemoveWordFromSentence(word, index);
-                Unhighlight(ButtonImage3);
-                
-            }
+            savedSentences.Add(SentenceTextBox.Text);
+            initialiser.SaveSentencesToFile(savedSentences);
         }
 
         private async void PlaySound_Button_Click(object sender, RoutedEventArgs e)
@@ -241,7 +154,181 @@ namespace EyeTalk
             await Task.Run(() => Speak(text));
         }
 
-        private void AddWordToSentence(string word, int index)
+        private void Image1_Button_Click(object sender, RoutedEventArgs e)
+        {
+            int index = 0;
+            SelectImage(index);
+        }
+
+        private void Image2_Button_Click(object sender, RoutedEventArgs e)
+        {
+            int index = 1;
+            SelectImage(index);
+        }
+
+        private void Image3_Button_Click(object sender, RoutedEventArgs e)
+        {
+            int index = 2;
+            SelectImage(index);
+           
+        }
+
+        private void Image4_Button_Click(object sender, RoutedEventArgs e)
+        {
+            int index = 3;
+            SelectImage(index);
+
+        }
+
+        private void Image5_Button_Click(object sender, RoutedEventArgs e)
+        {
+            int index = 4;
+            SelectImage(index);
+
+        }
+
+        private void Image6_Button_Click(object sender, RoutedEventArgs e)
+        {
+            int index = 5;
+            SelectImage(index);
+
+        }
+
+        private void Image7_Button_Click(object sender, RoutedEventArgs e)
+        {
+            int index = 6;
+            SelectImage(index);
+
+        }
+
+        private void Image8_Button_Click(object sender, RoutedEventArgs e)
+        {
+            int index = 7;
+            SelectImage(index);
+
+        }
+
+        private void Image9_Button_Click(object sender, RoutedEventArgs e)
+        {
+            int index = 8;
+            SelectImage(index);
+
+        }
+
+        private void Image10_Button_Click(object sender, RoutedEventArgs e)
+        {
+            int index = 9;
+            SelectImage(index);
+
+        }
+
+        private void Image11_Button_Click(object sender, RoutedEventArgs e)
+        {
+            int index = 10;
+            SelectImage(index);
+
+        }
+
+        private void Image12_Button_Click(object sender, RoutedEventArgs e)
+        {
+            int index = 11;
+            SelectImage(index);
+
+        }
+
+        private void Image13_Button_Click(object sender, RoutedEventArgs e)
+        {
+            int index = 12;
+            SelectImage(index);
+
+        }
+
+        private void Image14_Button_Click(object sender, RoutedEventArgs e)
+        {
+            int index = 13;
+            SelectImage(index);
+
+        }
+
+        private void Image15_Button_Click(object sender, RoutedEventArgs e)
+        {
+            int index = 14;
+            SelectImage(index);
+
+
+        }
+
+        private void CreateCategory(KeyValuePair<String, List<Picture>> category)
+        {
+            CategoryTitle.Text = category.Key;
+            categoryData = category.Value;
+            var numberOfPictures = category.Value.Count;
+            var emptyPictures = numberOfPictures - 15;
+            if (numberOfPictures > 0)
+            {
+                for (int i = 0; i < numberOfPictures; i++)
+                {
+                    UpdateImage(i);
+                }
+
+                for (int i = numberOfPictures; i < 15; i++)
+                {
+                    HideImage(i);
+                }
+            }
+            else
+            {
+                for (int i = 0; i < images.Count; i++)
+                {
+                    HideImage(i);
+                }
+            }
+        }
+
+        private void SelectImage(int i)
+        {
+            var word = textBlocks.ElementAt(i).Text;
+            var selected = categoryData.ElementAt(i).Selected;
+
+            if (amountSelected < 3 && selected == false)
+            {
+                AddWordToSentence(word, i);
+                Highlight(buttons.ElementAt(i));
+            }
+            else if (selected == true)
+            {
+                RemoveWordFromSentence(word, i);
+                Unhighlight(buttons.ElementAt(i));
+
+            }
+        }
+
+        private void HideImage(int i)
+        {
+            buttons.ElementAt(i).Visibility = Visibility.Hidden;
+            textBlocks.ElementAt(i).Text = "";
+            Unhighlight(buttons.ElementAt(i));
+        }
+
+        private void UpdateImage(int i)
+        {
+            var filepath = categoryData.ElementAt(i).FilePath;
+            var pictureName = categoryData.ElementAt(i).Name;
+            buttons.ElementAt(i).Visibility = Visibility.Visible;
+            images.ElementAt(i).Source = new BitmapImage(new Uri(filepath));
+            textBlocks.ElementAt(i).Text = pictureName;
+
+            if (categoryData.ElementAt(i).Selected == false)
+            {
+                Unhighlight(buttons.ElementAt(i));
+            }
+            else
+            {
+                Highlight(buttons.ElementAt(i));
+            }
+        }
+
+        private void AddWordToSentence(string word, int i)
         {
             sentence.Add(word, word);
             StringBuilder sb = new StringBuilder();
@@ -253,11 +340,11 @@ namespace EyeTalk
 
             amountSelected++;
 
-            categoryData.ElementAt(index).Selected = true;
+            categoryData.ElementAt(i).Selected = true;
            
         }
 
-        private void RemoveWordFromSentence(string word, int index)
+        private void RemoveWordFromSentence(string word, int i)
         {
             sentence.Remove(word);
             StringBuilder sb = new StringBuilder();
@@ -269,7 +356,7 @@ namespace EyeTalk
 
             amountSelected--;
 
-            categoryData.ElementAt(index).Selected = false;
+            categoryData.ElementAt(i).Selected = false;
         }
 
         private void Speak(string text)
@@ -286,15 +373,10 @@ namespace EyeTalk
         private void Unhighlight(Button ButtonImage)
         {
             ButtonImage.BorderBrush = new SolidColorBrush(Colors.Black);
-            ButtonImage.BorderThickness = new Thickness(3, 3, 3, 3);
+            ButtonImage.BorderThickness = new Thickness(1, 1, 1, 1);
 
         }
 
-        private void Save_Button_Click(object sender, RoutedEventArgs e)
-        {
-            savedSentences.Add(SentenceTextBox.Text);
-            initialiser.SaveSentencesToFile(savedSentences);
-        }
 
         /*
         Saved Sentences
@@ -389,27 +471,95 @@ namespace EyeTalk
             Microsoft.Win32.OpenFileDialog dialog = new Microsoft.Win32.OpenFileDialog
             {
                 DefaultExt = ".png",
-                Filter = "JPG Files (*.jpg)|*.jpeg|PNG Files (*.png)|*.png|JPEG Files (*.jpeg)|*.jpg|GIF Files (*.gif)|*.gif"
+                Filter = "JPG Files (*.jpg)|*.jpg|PNG Files (*.png)|*.png|JPEG Files (*.jpeg)|*.jpeg|GIF Files (*.gif)|*.gif"
             };
 
             Nullable<bool> result = dialog.ShowDialog();
 
             List<BitmapImage> list = new List<BitmapImage>();
 
-            if (result == true)
+            if (result == true && customPictures.Count <= 15)
             {
-                string filePath = dialog.FileName;
-                string name = System.IO.Path.GetFileNameWithoutExtension(dialog.FileName);
-                CustomFilePath.Text = name;
-                var uri = new Uri(filePath);
+                CustomFilePath.Text = dialog.FileName;
+                CustomName.Text = System.IO.Path.GetFileNameWithoutExtension(dialog.FileName);
+                var uri = new Uri(dialog.FileName);
                 var bitmap = new BitmapImage(uri);
                 CustomImage.Source = bitmap;
              }
+        }
+
+        private void SaveCustomPicture()
+        {
+            Picture customPicture = new Picture(CustomName.Text, false, CustomFilePath.Text);
+            if (customPictures.Count < 15)
+            {
+                customPictures.Add(customPicture);
+                Status.Text = "Added picture";
+                initialiser.SaveCustomPictureToFile(customPictures);
+            }
+            else if (customPictures.Contains(customPicture))
+            {
+                Status.Text = "This image has already been added.";
+            }
+            else
+            {
+                Status.Text = "Cannot add more than 15 pictures";
+            }
         }
 
         private void Add_Custom_Picture_Button_Click(object sender, RoutedEventArgs e)
         {
             LoadCustomPicture();
         }
+
+        private void Save_Custom_Picture_Button_Click(object sender, RoutedEventArgs e)
+        {
+            SaveCustomPicture();
+        }
+
+        private async void Fast_Button_Click(object sender, RoutedEventArgs e)
+        {
+            synth.Rate = 3;
+            SpeedStatus.Text = "Fast";
+            await Task.Run(() => Speak("This is an example of my voice."));
+
+        }
+
+        private async void Slow_Button_Click(object sender, RoutedEventArgs e)
+        {
+            synth.Rate = -3;
+            SpeedStatus.Text = "Slow";
+            await Task.Run(() => Speak("This is an example of my voice."));
+
+        }
+
+        private async void Normal_Button_Click(object sender, RoutedEventArgs e)
+        {
+            synth.Rate = 0;
+            SpeedStatus.Text = "Normal";
+            await Task.Run(() => Speak("This is an example of my voice."));
+        }
+
+        private async void Male_Button_Click(object sender, RoutedEventArgs e)
+        {
+            IReadOnlyCollection<InstalledVoice> InstalledVoices = synth.GetInstalledVoices();
+            InstalledVoice InstalledVoice = InstalledVoices.ElementAt(1);
+            synth.SelectVoice(InstalledVoice.VoiceInfo.Name);
+            GenderStatus.Text = "Male";
+            await Task.Run(() => Speak("This is an example of my voice."));
+    
+
+        }
+
+        private async void Female_Button_Click(object sender, RoutedEventArgs e)
+        {
+            synth.SelectVoiceByHints(VoiceGender.Female, VoiceAge.Adult);
+            GenderStatus.Text = "Female";
+            await Task.Run(() => Speak("This is an example of my voice."));
+         
+
+        }
+
+       
     }
 }
--- EyeTalk/Objects/SaveFileSerialiser.cs
+++ EyeTalk/Objects/SaveFileSerialiser.cs
@@ -9,63 +9,70 @@ namespace EyeTalk.Objects
 {
     class SaveFileSerialiser
     {
-        static Picture Pizza = new Picture("Pizza", false, "C:\\Users\\User\\source\\repos\\CategoryAndPicture\\CategoryAndPicture\\Pictures\\pizza.png");
-        static Picture Apple = new Picture("Apple", false, "C:\\Users\\User\\source\\repos\\CategoryAndPicture\\CategoryAndPicture\\Pictures\\apple.png");
-        static Picture Banana = new Picture("Banana", false, "C:\\Users\\User\\source\\repos\\CategoryAndPicture\\CategoryAndPicture\\Pictures\\banana.png");
+        static Picture pizza = new Picture("Pizza", false, "C:\\Users\\User\\source\\repos\\CategoryAndPicture\\CategoryAndPicture\\Pictures\\pizza.png");
+        static Picture apple = new Picture("Apple", false, "C:\\Users\\User\\source\\repos\\CategoryAndPicture\\CategoryAndPicture\\Pictures\\apple.png");
+        static Picture banana = new Picture("Banana", false, "C:\\Users\\User\\source\\repos\\CategoryAndPicture\\CategoryAndPicture\\Pictures\\banana.png");
 
-        static Picture Happy = new Picture("Happy", false, "C:\\Users\\User\\source\\repos\\CategoryAndPicture\\CategoryAndPicture\\Pictures\\happy.png");
-        static Picture Sad = new Picture("Sad", false, "C:\\Users\\User\\source\\repos\\CategoryAndPicture\\CategoryAndPicture\\Pictures\\sad.png");
-        static Picture Angry = new Picture("Angry", false, "C:\\Users\\User\\source\\repos\\CategoryAndPicture\\CategoryAndPicture\\Pictures\\angry.png");
+        static Picture happy = new Picture("Happy", false, "C:\\Users\\User\\source\\repos\\CategoryAndPicture\\CategoryAndPicture\\Pictures\\happy.png");
+        static Picture sad = new Picture("Sad", false, "C:\\Users\\User\\source\\repos\\CategoryAndPicture\\CategoryAndPicture\\Pictures\\sad.png");
+        static Picture angry = new Picture("Angry", false, "C:\\Users\\User\\source\\repos\\CategoryAndPicture\\CategoryAndPicture\\Pictures\\angry.png");
 
-        static Picture Shower = new Picture("Shower", false, "C:\\Users\\User\\source\\repos\\CategoryAndPicture\\CategoryAndPicture\\Pictures\\shower.png");
-        static Picture Toilet = new Picture("Toilet", false, "C:\\Users\\User\\source\\repos\\CategoryAndPicture\\CategoryAndPicture\\Pictures\\toilet.png");
-        static Picture WashHands = new Picture("Wash Hands", false, "C:\\Users\\User\\source\\repos\\CategoryAndPicture\\CategoryAndPicture\\Pictures\\washhands.png");
+        static Picture shower = new Picture("Shower", false, "C:\\Users\\User\\source\\repos\\CategoryAndPicture\\CategoryAndPicture\\Pictures\\shower.png");
+        static Picture toilet = new Picture("Toilet", false, "C:\\Users\\User\\source\\repos\\CategoryAndPicture\\CategoryAndPicture\\Pictures\\toilet.png");
+        static Picture washHands = new Picture("Wash Hands", false, "C:\\Users\\User\\source\\repos\\CategoryAndPicture\\CategoryAndPicture\\Pictures\\washhands.png");
+
+        static Picture I = new Picture("Wash Hands", false, "C:\\Users\\User\\source\\repos\\CategoryAndPicture\\CategoryAndPicture\\Pictures\\washhands.png");
+        static Picture me = new Picture("Wash Hands", false, "C:\\Users\\User\\source\\repos\\CategoryAndPicture\\CategoryAndPicture\\Pictures\\washhands.png");
+        static Picture you = new Picture("Wash Hands", false, "C:\\Users\\User\\source\\repos\\CategoryAndPicture\\CategoryAndPicture\\Pictures\\washhands.png");
+        static Picture him = new Picture("Wash Hands", false, "C:\\Users\\User\\source\\repos\\CategoryAndPicture\\CategoryAndPicture\\Pictures\\washhands.png");
+        static Picture her = new Picture("Wash Hands", false, "C:\\Users\\User\\source\\repos\\CategoryAndPicture\\CategoryAndPicture\\Pictures\\washhands.png");
+        static Picture it = new Picture("Wash Hands", false, "C:\\Users\\User\\source\\repos\\CategoryAndPicture\\CategoryAndPicture\\Pictures\\washhands.png");
+        static Picture they = new Picture("Wash Hands", false, "C:\\Users\\User\\source\\repos\\CategoryAndPicture\\CategoryAndPicture\\Pictures\\washhands.png");
+        static Picture he = new Picture("Wash Hands", false, "C:\\Users\\User\\source\\repos\\CategoryAndPicture\\CategoryAndPicture\\Pictures\\washhands.png");
+        static Picture she = new Picture("Wash Hands", false, "C:\\Users\\User\\source\\repos\\CategoryAndPicture\\CategoryAndPicture\\Pictures\\washhands.png");
+        static Picture we = new Picture("Wash Hands", false, "C:\\Users\\User\\source\\repos\\CategoryAndPicture\\CategoryAndPicture\\Pictures\\washhands.png");
 
 
-        static List<Picture> food = new List<Picture>() { Pizza, Apple, Banana };
-        static List<Picture> emotions = new List<Picture>() { Happy, Sad, Angry };
-        static List<Picture> bathroom = new List<Picture>() { Shower, Toilet, WashHands };
+
+        static List<Picture> food = new List<Picture>() { pizza, apple, banana, pizza, pizza, pizza, pizza, pizza, pizza, pizza, pizza };
+        static List<Picture> emotions = new List<Picture>() { happy, sad, angry, happy, happy, happy, happy, happy, happy, happy, happy, happy };
+        static List<Picture> bathroom = new List<Picture>() { shower, toilet, washHands, shower, shower, shower, shower, shower};
+        static List<Picture> verbs = new List<Picture>() { shower, toilet, washHands, shower, shower, shower, shower, shower };
+        static List<Picture> pronouns = new List<Picture>() { shower, toilet, washHands, shower, shower, shower, shower, shower };
+
+
+        static List<Picture> custom = new List<Picture>() { };
+
 
         static SortedList<String, List<Picture>> categories = new SortedList<string, List<Picture>>(){
             {"Foods", food },
             {"Bathroom", bathroom },
-            {"Emotions", emotions }
+            {"Emotions", emotions },
+            {"Verbs", verbs},
+            {"Pronouns", pronouns },
+            {"Custom", custom }
             };
 
         static List<string> savedSentences = new List<string>();
 
 
+
+
         public static string dir = @"C:\Users\User\Desktop\SaveFolder";
         string categoryPath = Path.Combine(dir, "Categories.bin");
         string savedSentencesPath = Path.Combine(dir, "SavedSentences.bin");
+        string customPicturesPath = Path.Combine(dir, "CustomPictures.bin");
+
 
 
         public SaveFileSerialiser()
         {
-            CreateCategoryFile();
+           // CreateCategoryFile();
             //CreateSavedSentencesFile();
+            CreateCustomPictureFile();
         }
 
         //load category from a file
-        public SortedList<String, List<Picture>> LoadCategories()
-        {
-            using (Stream stream = File.Open(categoryPath, FileMode.Open))
-            {
-                var bformatter = new System.Runtime.Serialization.Formatters.Binary.BinaryFormatter();
-
-                return (SortedList<String, List<Picture>>)bformatter.Deserialize(stream);
-            }
-        }
-
-        public List<string> LoadSentences()
-        {
-            using (Stream stream = File.Open(savedSentencesPath, FileMode.Open))
-            {
-                var bformatter = new System.Runtime.Serialization.Formatters.Binary.BinaryFormatter();
-
-                return (List<string>)bformatter.Deserialize(stream);
-            }
-        }
 
         public void CreateCategoryFile()
         {
@@ -74,6 +81,16 @@ namespace EyeTalk.Objects
                 var bformatter = new System.Runtime.Serialization.Formatters.Binary.BinaryFormatter();
 
                 bformatter.Serialize(stream, categories);
+            }
+        }
+
+        public SortedList<String, List<Picture>> LoadCategories()
+        {
+            using (Stream stream = File.Open(categoryPath, FileMode.Open))
+            {
+                var bformatter = new System.Runtime.Serialization.Formatters.Binary.BinaryFormatter();
+
+                return (SortedList<String, List<Picture>>)bformatter.Deserialize(stream);
             }
         }
 
@@ -87,6 +104,16 @@ namespace EyeTalk.Objects
             }
         }
 
+        public List<string> LoadSentences()
+        {
+            using (Stream stream = File.Open(savedSentencesPath, FileMode.Open))
+            {
+                var bformatter = new System.Runtime.Serialization.Formatters.Binary.BinaryFormatter();
+
+                return (List<string>)bformatter.Deserialize(stream);
+            }
+        }
+
         public void SaveSentencesToFile(List<string> sentences)
         {
             using (Stream stream = File.Open(savedSentencesPath, FileMode.Create))
@@ -97,9 +124,34 @@ namespace EyeTalk.Objects
             }
         }
 
+        public void CreateCustomPictureFile()
+        {
+            using (Stream stream = File.Open(customPicturesPath, FileMode.Create))
+            {
+                var bformatter = new System.Runtime.Serialization.Formatters.Binary.BinaryFormatter();
 
+                bformatter.Serialize(stream, custom);
+            }
+        }
 
+        public List<Picture> LoadCustomPictures()
+        {
+            using (Stream stream = File.Open(customPicturesPath, FileMode.Open))
+            {
+                var bformatter = new System.Runtime.Serialization.Formatters.Binary.BinaryFormatter();
+
+                return (List<Picture>)bformatter.Deserialize(stream);
+            }
+        }
+
+        public void SaveCustomPictureToFile(List<Picture> custom)
+        {
+            using (Stream stream = File.Open(customPicturesPath, FileMode.Create))
+            {
+                var bformatter = new System.Runtime.Serialization.Formatters.Binary.BinaryFormatter();
+
+                bformatter.Serialize(stream, custom);
+            }
+        }
     }
-
-
 }
