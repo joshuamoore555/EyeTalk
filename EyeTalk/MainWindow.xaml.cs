5c813637b18a6657dbd200f0d54cfacdb831b69a
Joshua Moore <Joshua Moore@GARCON>
2018-03-04 17:25:59 +0000

    changed objects to util

--- EyeTalk/MainWindow.xaml.cs
+++ EyeTalk/MainWindow.xaml.cs
@@ -1,5 +1,4 @@
-﻿using EyeTalk.Objects;
-using System;
+﻿using System;
 using System.Threading;
 using System.Collections.Generic;
 using System.Linq;
--- EyeTalk/Utils/SaveFileSerialiser.cs
+++ EyeTalk/Utils/SaveFileSerialiser.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace EyeTalk.Objects
+namespace EyeTalk.Utils
 {
     class SaveFileSerialiser
     {
