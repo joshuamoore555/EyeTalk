57964c955aa094b98d16c3644991e15df06e12c0
Joshua <joshuamoore555@gmail.com>
2018-03-24 01:46:29 +0000

    Added tests

---
+++ EyeTalk/Tests/HomeTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestStack.White;
+using TestStack.White.UIItems;
+using TestStack.White.Factory;
+
+namespace EyeTalk.Tests
+{
+    [TestClass]
+   public class HomeTests
+    {
+        [TestMethod]
+        public void TestHome()
+        {
+            var app = Application.Launch("EyeTalk");
+            var window = app.GetWindow("MainWindow",InitializeOption.NoCache);
+            var begin = window.Get<Button>("BeginSpeaking");
+            app.Close();
+        }
+    }
+}
